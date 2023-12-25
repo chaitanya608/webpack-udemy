b64793023fc29601e67e09975f8dba703c9f1349
chaitanya608 <chaitanya608@gmail.com>
2023-12-25 18:54:10 +0530

    completed Modules

---
+++ Modules/functions/add.js
@@ -0,0 +1,15 @@
+function add(a, b) {
+	return a + b;
+}
+
+function addAndMultiply(a, b, c) {
+	return (a + b) * c;
+}
+
+// module.exports = {
+// 	add,
+// 	addAndMultiply
+// };
+
+// module.exports = add;
+export default add;
---
+++ Modules/main.js
@@ -0,0 +1,6 @@
+import * as $ from "./functions";
+
+const output = $.add(1, 2);
+console.log({
+	output
+});
