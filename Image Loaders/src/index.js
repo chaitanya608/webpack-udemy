28dcf01489f91ceabb952a390a98fa17d25cd603
chaitanya608 <chaitanya608@gmail.com>
2023-12-30 17:05:04 +0530

    image loaders

---
+++ Image Loaders/src/index.js
@@ -0,0 +1,28 @@
+import _ from "lodash";
+import styles from "./index.scss";
+import "./clearButton";
+import webpackLogo from "./assets/webpack_logo.png";
+
+console.log({
+	styles,
+	webpackLogo
+});
+
+const btn1 = document.getElementById("button1");
+const logoEl = document.getElementById("logo");
+
+btn1.addEventListener("click", function() {
+	const el = document.getElementById("header");
+	el.innerHTML = "Hey, I have updated the code!";
+
+	const listItems = ["Apple", "Orange", "Banana"];
+	const ul = document.getElementById("shopping-list");
+	_.forEach(listItems, item => {
+		const tempEl = document.createElement("li");
+		tempEl.innerHTML = item;
+		ul.appendChild(tempEl);
+	});
+});
+
+btn1.classList.add([styles.button]);
+logoEl.src = webpackLogo;
